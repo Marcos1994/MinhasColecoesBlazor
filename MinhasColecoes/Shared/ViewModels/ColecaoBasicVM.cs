2b872ef602fd8ccf2327ab6b5a4429b96bd3031c
Marcos <marcos.antas@hotmail.com.br>
2022-01-11 17:12:52 -0300

    Listagem de coleções base

---
+++ MinhasColecoes/Shared/ViewModels/ColecaoBasicVM.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinhasColecoes.Shared.ViewModels
+{
+	public class ColecaoBasicVM
+	{
+		public int Id { get; set; }
+		public string Nome { get; set; }
+		public string Descricao { get; set; }
+		public string Foto { get; set; }
+		public bool Publica { get; set; }
+		public int IdDono { get; set; }
+		public int? IdColecaoMaior { get; set; }
+		public int QuantidadeMembros { get; set; }
+		public int QuantidadeSubcolecoes { get; set; }
+		public bool UsuarioParticipa { get; set; }
+	}
+}
