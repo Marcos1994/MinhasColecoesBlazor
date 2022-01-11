5b90ac2c239c7ee5559e032f4a494e3bdc15e0f4
Marcos <marcos.antas@hotmail.com.br>
2022-01-11 18:41:17 -0300

    Consumindo API

--- MinhasColecoes/Client/Program.cs
+++ MinhasColecoes/Client/Program.cs
@@ -17,7 +17,8 @@ namespace MinhasColecoes.Client
 			var builder = WebAssemblyHostBuilder.CreateDefault(args);
 			builder.RootComponents.Add<App>("#app");
 
-			builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+			builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:5001") });
+			//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 			await builder.Build().RunAsync();
 		}
