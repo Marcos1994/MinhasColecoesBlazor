df2049e3ef22be3733b96825c617186210427e33
Marcos <marcos.antas@hotmail.com.br>
2022-01-15 00:23:17 -0300

    Centralização da autenticação
    A verificação do token agora é feita pelo HttpClientHelper, que retorna o cliente com o token no campo de autorização do cabeçalho sempre que este estiver presente no local storage.

---
+++ MinhasColecoes/Client/MinhasColecoesAPI/UsuarioAPI.cs
@@ -0,0 +1,14 @@
+﻿using Blazored.LocalStorage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace MinhasColecoes.Client.MinhasColecoesAPI
+{
+	public class UsuarioAPI
+	{
+
+	}
+}
--- MinhasColecoes/Client/Program.cs
+++ MinhasColecoes/Client/Program.cs
@@ -9,6 +9,7 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Blazored.LocalStorage;
+using MinhasColecoes.Client.MinhasColecoesAPI;
 
 namespace MinhasColecoes.Client
 {
@@ -26,6 +27,8 @@ namespace MinhasColecoes.Client
 			//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 			builder.Services.AddBlazoredLocalStorage();
+
+			builder.Services.AddScoped<HttpClientHelper>();
 
 			await builder.Build().RunAsync();
 		}
