62dba8cb4de8f78ed8f2a15d44f083ae7ea1aea3
Marcos <marcos.antas@hotmail.com.br>
2022-01-30 20:54:44 -0300

    Refatoração do sistema de exibição de erros de validação

---
+++ MinhasColecoes/Shared/ErrorModels/ValidationErrorModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinhasColecoes.Shared.ErrorModels
+{
+	public class ValidationErrorModel
+	{
+		public string Title { get; set; }
+		public Dictionary<string, string[]> Errors { get; set; } = new Dictionary<string, string[]>();
+	}
+}
