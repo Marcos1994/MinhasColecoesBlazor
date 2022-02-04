93591272348b831bfe8b6b41adf918c3c10e1420
Marcos <marcos.antas@hotmail.com.br>
2022-02-04 01:08:38 -0300

    Criação da classe estática RelacaoUsuarioItemHelper
    Esta classe possui métodos que retornam o nome da relação ou o ícone do bootstrap de acordo com o enum.

---
+++ MinhasColecoes/Client/Models/RelacaoUsuarioItemHelper.cs
@@ -0,0 +1,49 @@
+﻿using MinhasColecoes.Shared.Enums;
+
+namespace MinhasColecoes.Client.Models
+{
+	public static class RelacaoUsuarioItemHelper
+	{
+		public static string Nome(EnumRelacaoUsuarioItem Relacao)
+		{
+			switch (Relacao)
+			{
+				case EnumRelacaoUsuarioItem.Desejo:
+					return "Desejo";
+				case EnumRelacaoUsuarioItem.AguardandoRecebimento:
+					return "Aguardando Recebimento";
+				case EnumRelacaoUsuarioItem.Possuo:
+					return "Possuo";
+				case EnumRelacaoUsuarioItem.Trocar:
+					return "Possuo para troca";
+				case EnumRelacaoUsuarioItem.Vender:
+					return "Possuo para vender";
+				case EnumRelacaoUsuarioItem.JaPossui:
+					return "Ja tive";
+				default:
+					return "Não possuo";
+			}
+		}
+
+		public static string Icone(EnumRelacaoUsuarioItem Relacao)
+		{
+			switch (Relacao)
+			{
+				case EnumRelacaoUsuarioItem.Desejo:
+					return "bi-heart";
+				case EnumRelacaoUsuarioItem.AguardandoRecebimento:
+					return "bi-heart-half";
+				case EnumRelacaoUsuarioItem.Possuo:
+					return "bi-heart-fill";
+				case EnumRelacaoUsuarioItem.Trocar:
+					return "bi-arrow-left-right";
+				case EnumRelacaoUsuarioItem.Vender:
+					return "bi-currency-dollar";
+				case EnumRelacaoUsuarioItem.JaPossui:
+					return "bi-hourglass";
+				default:
+					return "bi-x-circle";
+			}
+		}
+	}
+}
