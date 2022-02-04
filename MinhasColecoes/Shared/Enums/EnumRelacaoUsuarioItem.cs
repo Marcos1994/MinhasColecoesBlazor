0813ea994aef387cc8e124f6221c558a13070859
Marcos <marcos.antas@hotmail.com.br>
2022-02-04 01:16:16 -0300

    Atualização do CardItem e do cadastro de multiplos itens
    Agora a listagem das relações é feita utilizando a classe RelacaoUsuarioItemHelper

--- MinhasColecoes/Shared/Enums/EnumRelacaoUsuarioItem.cs
+++ MinhasColecoes/Shared/Enums/EnumRelacaoUsuarioItem.cs
@@ -4,25 +4,12 @@ namespace MinhasColecoes.Shared.Enums
 {
 	public enum EnumRelacaoUsuarioItem
 	{
-		[Display(Name ="Não possuo")]
 		NaoPossuo = 0,
-
-		[Display(Name = "Desejo")]
 		Desejo = 1,
-
-		[Display(Name = "Aguardando")]
 		AguardandoRecebimento = 2,
-
-		[Display(Name = "Possuo")]
 		Possuo = 3,
-
-		[Display(Name = "Vendo")]
 		Vender = 4,
-
-		[Display(Name = "Tenho Para Troca")]
 		Trocar = 5,
-
-		[Display(Name = "Já possuí")]
 		JaPossui = 6
 	}
 }
