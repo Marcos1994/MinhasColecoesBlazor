2b5dd0b7f3f72dea7fdaca19ca4de66fbe5a57c3
Marcos <marcos.antas@hotmail.com.br>
2022-02-12 18:02:42 -0300

    Atualização na geração de nome do item de acordo com o arquivo.
    Agora cada palavra do nome do arquivo ficará com a primeira letra maiuscula.

---
+++ MinhasColecoes/Client/Util/Formatar.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MinhasColecoes.Client.Util
+{
+	public static class Formatar
+	{
+		public static string PrimeiroCaractereMaiusculo(string texto)
+		{
+			if (texto.Length == 0)
+				return "";
+
+			string[] palavras = texto.Split(' ');
+			List<string> palavrasFormatadas = new List<string>();
+			foreach (string p in palavras)
+			{
+				if (p.Length == 0)
+					continue;
+				else if (p.Length == 1)
+					palavrasFormatadas.Add(char.ToUpper(p[0]).ToString());
+				else
+					palavrasFormatadas.Add(char.ToUpper(p[0]) + p.Substring(1));
+			}
+			return string.Join(' ', palavrasFormatadas);
+		}
+	}
+}
